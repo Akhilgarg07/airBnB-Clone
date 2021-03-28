3cea5e457e97fd335fec7071fe556cd1bb2d933f
Akhilgarg07 <akhilgarg420@gmail.com>
2021-03-28 13:11:40 +0530

    created room app model and it's admin

--- rooms/models.py
+++ rooms/models.py
@@ -3,6 +3,50 @@ from django_countries.fields import CountryField
 from core import models as core_models
 from users import models as user_models
 
+class AbstractItem(core_models.TimeStampedModel):
+
+        """Abstract Item"""
+
+        name = models.CharField(max_length=80)
+
+        class Meta:
+            abstract = True
+
+        def __str__(self):
+            return self.name
+
+
+class Amenity(AbstractItem):
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+class RoomType(AbstractItem):
+
+    class Meta:
+        verbose_name = "Room Type"
+
+class Facility(AbstractItem):
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+class HouseRule(AbstractItem):
+
+    class Meta:
+        verbose_name = "House Rule"
+
+class Photo(core_models.TimeStampedModel):
+
+    """Photo Model Definiton"""
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
+
 class Room(core_models.TimeStampedModel):
 
     """Room Model Definition"""
@@ -21,3 +65,10 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
+
+    def __str__(self):
+        return self.name
